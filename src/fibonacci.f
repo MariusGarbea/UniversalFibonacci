b6a8ef3745789388ad0f4ddfe730a4c0ef018e7d
Rachel <chichesterracheldempsey@gmail.com>
2017-10-22 17:57:26 -0400

    added .f

---
+++ src/fibonacci.f
@@ -0,0 +1,14 @@
+PROGRAM    Fibonacci
+   IMPLICIT   NONE
+   INTEGER :: FIRST, SECOND, TEMP, IX
+   FIRST = 0
+   SECOND = 1
+   WRITE (*,*) FIRST
+   WRITE (*,*) SECOND
+   DO IX = 1, 45, 1
+      TEMP = FIRST + SECOND
+      FIRST = SECOND
+      SECOND = TEMP
+      WRITE (*,*) TEMP
+   END DO
+END PROGRAM Fibonacci
